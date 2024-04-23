baa0dffedfd0b5586798a0cc3a52ccd69892ff38
luke-d-moore <luke.d.moore1@gmail.com>
2024-04-23 22:32:06 +0100

    trying out simple add functions
    trying out simple add functions

--- FirstConsole/FirstConsole/Program.fs
+++ FirstConsole/FirstConsole/Program.fs
@@ -8,3 +8,16 @@ module Say =
 
 
 Say.hello("Luke")
+
+let addnumbers a b = a + b 
+
+let x = addnumbers 5 6
+
+printfn "%i" x
+
+let int2String (x: int) = string x
+
+let y = int2String x
+
+printfn "%s" y
+

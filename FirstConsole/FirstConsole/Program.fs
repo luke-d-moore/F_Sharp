b2bd4e04ad81a92ed8f869988710869b505d8192
luke-d-moore <luke.d.moore1@gmail.com>
2024-04-23 22:24:05 +0100

    initial commit
    initial commit

---
+++ FirstConsole/FirstConsole/Program.fs
@@ -0,0 +1,10 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
+
+
+Say.hello("Luke")
